e553b11a01c3c8ada732d91a97f61c3fdc034275
ryan bautista <96922150+xryanb@users.noreply.github.com>
2022-05-13 17:22:53 -0700

    Add files via upload

--- server/models/joke.model.js
+++ server/models/joke.model.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const JokeSchema = new mongoose.Schema({
-	"setup": String,
-	"punchline": String,
+	setup:{ 
+	type: String,
+	required:[true,'joke is required'],
+	minlength:[6,'must be six characters long']
+	},
+	punchline:{
+	type: String,
+	required:[true,'punchline is required!'],
+	minlength:[6,'must be 6 characters long!']
+	}
 });
 
 const Joke = mongoose.model("Joke", JokeSchema);
